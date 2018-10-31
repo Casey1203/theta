c5e28d1c52acd4397aa78b56e214134a9dbc2a7c
Jiaxin.Zhang <jiaxin.zhang@datayes.com>
2018-10-31 21:22:22 +0800

    add test

--- theta/boosting/adaboost.py
+++ theta/boosting/adaboost.py
@@ -3,7 +3,7 @@
 # @Author  : jia
 
 import numpy as np
-from stump import build_stump
+from stump import build_stump, stump_classify
 from theta.util.file_loader import load_simple_data
 
 def calculate_error_rate(G_m, data_input, class_label):
@@ -35,7 +35,7 @@ class Adaboost:
 			print 'iter_num: %s, total error rate: %s' % (m, aggregate_error_rate)
 			if aggregate_error_rate == 0.:
 				break
-
+		print self.G
 
 	def evaluate(self, data_input, class_label):
 		model_output = np.zeros(class_label.shape)
@@ -46,12 +46,14 @@ class Adaboost:
 		return error_rate
 
 	def predict(self, data_input):
-		N = np.shape(data_input)
-		model_output = np.zeros((N,))
-		for m in xrange(self.M):
-			model_output += self.alpha[m] * self.G[m](data_input)
-		model_output = np.sign(model_output)
-		return model_output
+		m = np.shape(data_input)[0]
+		num_stump = len(self.G)
+		aggregate_label = np.zeros((m, ))
+		for m in xrange(num_stump):
+			output_label = stump_classify(data_input, self.G[m]['dimension'], self.G[m]['threshold_value'], self.G[m]['threshold_inequality'])
+			aggregate_label += self.G[m]['alpha'] * output_label
+		return np.sign(aggregate_label)
+
 
 
 
@@ -61,7 +63,7 @@ def test():
 	data_input, class_label = load_simple_data()
 	model = Adaboost(100)
 	model.fit(data_input, class_label)
-
+	print model.predict(np.array([[5, 5], [0, 0]]))
 
 
 if __name__ == '__main__':
