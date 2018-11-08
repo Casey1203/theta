f6b5d99b7c1f7d0b25f3a040787c441d1ccdec36
Jiaxin.Zhang <jiaxin.zhang@datayes.com>
2018-11-08 19:20:48 +0800

    add tf

--- theta/mlds_hw/hw_0/cnn.py
+++ theta/mlds_hw/hw_0/cnn.py
@@ -3,31 +3,56 @@
 # @Author  : jia
 
 import tensorflow as tf
-from theta.util.file_loader import load_mnist_data
+from theta.util.file_loader import load_mnist_img_data, load_mnist_label_data, load_mnist_data
 
 
-def build_model(data_input, class_label):
+def train(data_input, class_label, num_epoch, batch_size):
+	# 定义placeholder
 	m, n = data_input.shape
-	X = tf.placeholder(shape=[None, m, n, None], name='data_input')
-	y = tf.placeholder(shape=[None, m, None, None], name='class_label')
-	
+	label_num = 10
+	X = tf.placeholder(dtype=tf.float32, shape=[None, n], name='data_input')
+	y = tf.placeholder(dtype=tf.float32, shape=[None, label_num], name='class_label')
+
+	# 定义variable
+	W = tf.Variable(tf.zeros([n, label_num]))
+	b = tf.Variable(tf.zeros([label_num]) + 0.1)
+
+	predict_y = tf.nn.softmax(tf.matmul(X, W) + b)
+	# 定义Loss function
+	cross_entropy = tf.reduce_mean(
+		tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predict_y)
+	)
+	# 定义optimizer
+	train_model = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cross_entropy)
+	# 开始训练
+
+	init = tf.initialize_all_variables()
+	with tf.Session() as sess:
+		sess.run(init)
+		batch_offset = 0
+		for i in xrange(num_epoch):
+			batch_x, batch_y = data_input[batch_offset: batch_offset + batch_size], class_label[batch_offset: batch_offset + batch_size]
+			sess.run(train_model, feed_dict={'X': batch_x, 'y': batch_y})
+			if i % 100 == 0:
+				correct_prediction = tf.equal(tf.argmax(predict_y, 1), tf.argmax(y, 1))
+				accuracy = tf.reduce_mean(correct_prediction)
+				print sess.run(accuracy, feed_dict=)
+		batch_offset += batch_size
 
 
-def train():
-	pass
-
-
-def evaluate():
+def evaluate(X, y):
 	pass
 
 
 
 def main():
-	x_file_name = '../../data/train-images-idx3-ubyte.gz'
-	y_file_name = '../../data/train-labels-idx1-ubyte.gz'
-	data_input, class_label = load_mnist_data(x_file_name, y_file_name)
-	print data_input
-	print class_label
+	# x_file_name = '../../data/train-images-idx3-ubyte.gz'
+	# y_file_name = '../../data/train-labels-idx1-ubyte.gz'
+	# data_input, x_head = load_mnist_img_data(x_file_name)
+	# class_label, y_head = load_mnist_label_data(y_file_name)
+	file_name = '../../data/mnist.npz'
+	x_train, y_train, x_test, y_test = load_mnist_data(file_name)
+
 
 
 if __name__ == '__main__':
--- theta/util/file_loader.py
+++ theta/util/file_loader.py
@@ -3,7 +3,8 @@
 # @Author  : jia
 
 import numpy as np
-import gzip
+import gzip, struct
+import matplotlib.pyplot as plt
 
 def load_dataset(file_name):
 	data_mat = []
@@ -40,18 +41,51 @@ def load_horse_colic_dataset(file_name):
 	return data_arr, label_arr
 
 
-def load_mnist_data(x_file_name, y_file_name):
-	data_mat = []
-	label_mat = []
-	with gzip.open(x_file_name, 'rb') as fr:
-		for line in fr.readlines():
-			print line
-			data_mat.append(line)
+def load_mnist_img_data(x_file_name):
+	bin_file = open(x_file_name, 'rb') # 读取二进制文件
+	buffers = bin_file.read()
+	head = struct.unpack_from('>IIII', buffers, 0) # 读取前4个整数，返回一个元祖
+	offset = struct.calcsize('>IIII') # 定位到data开始的位置
 
-	with gzip.open(y_file_name, 'rb') as fr:
-		for line in fr.readlines():
-			print line
-			label_mat.append(line)
-	data_arr = np.array(data_mat)
-	label_arr = np.array(label_mat)
-	return data_arr, label_arr
+	img_num = head[1]
+
+	fig = plt.figure()
+	magic, numImages, imgRows, imgCols = struct.unpack_from(">IIII", buffers, 0)
+	data_input = []
+	for i in xrange(img_num):
+		print i
+		tmp = struct.unpack_from('>784B', buffers, offset)
+		im = np.array(tmp).reshape(28, 28)
+		fig.add_subplot(111)
+		plt.imshow(im, cmap='gray')
+		plt.show()
+		data_input.append(im)
+		offset += struct.calcsize('>784B')
+	data_input = np.array(data_input)
+	bin_file.close()
+
+	return data_input, head
+
+def load_mnist_label_data(y_file_name):
+	bin_file = open(y_file_name, 'rb') # 读取二进制文件
+	buffers = bin_file.read()
+	head = struct.unpack_from('>II', buffers, 0) # 读取前2个整数
+	label_num = head[1]
+	offset = struct.calcsize('>II') # 定位到label开始的位置
+	num_string = '>' + str(label_num) + 'B' # fmt格式：'>60000B'
+	labels = struct.unpack_from(num_string, buffers, offset) # 取label数据
+	bin_file.close()
+	labels = np.reshape(labels, [label_num]) # 转型为列表(一维数组)
+	return labels, head
+
+
+def load_mnist_data(file_name):
+	f = np.load(file_name)
+	x_train, y_train = f['x_train'], f['y_train']
+	x_test, y_test = f['x_test'], f['y_test']
+	x_num, x_width, x_height = x_train.shape
+	x_train = x_train.reshape([x_num, x_width * x_height])
+	x_num, x_width, x_height = x_test.shape
+	x_test = x_test.reshape([x_num, x_width * x_height])
+	f.close()
+	return x_train, y_train, x_test, y_test
