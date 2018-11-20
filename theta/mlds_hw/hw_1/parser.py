62e17081f1059ad17d21085907e23f08c75c0a8f
Jiaxin.Zhang <jiaxin.zhang@datayes.com>
2018-11-20 18:03:17 +0800

    add train

--- theta/mlds_hw/hw_1/parser.py
+++ theta/mlds_hw/hw_1/parser.py
@@ -2,9 +2,9 @@
 # @Time    : 18-11-19 下午2:16
 # @Author  : jia
 
-from reader import load_holmes_raw_data
 import os
 import numpy as np
+from collections import Counter
 
 def sentence_parser(sentence, min_length, stop_word):
 	word_list = sentence.split()
@@ -17,16 +17,44 @@ def sentence_parser(sentence, min_length, stop_word):
 	mid_word = word_list[mid_index]
 	return 1, forward_sentence, backward_sentence, mid_word
 
-
-def embedded_sentence(sentence, stop_word, word_vector, word_vector_size):
+def clean_sentence(sentence, stop_word):
 	word_list = sentence.split()
+	word_list = [word for word in word_list if word not in stop_word]
+	return ' '.join(word_list)
+
+def embedded_sentence_by_word2vec(sentence, stop_word, word_vector, word_vector_size, min_length):
+	word_list = sentence.split()
+	word_list = [word for word in word_list if word not in stop_word]
 	embedded_list = []
+	if len(word_list) < min_length:
+		return embedded_list
 	for word in word_list:
 		if word in word_vector and word not in stop_word:
 			embedded_list.append(word_vector[word])
 		else:
 			embedded_list.append(np.zeros((word_vector_size,)))
 	return embedded_list
+
+def embedded_sentence_by_id(sentence, vocab_map_id, seq_length):
+	word_list = sentence.split()
+	id_s = np.ones(seq_length, np.int32) * vocab_map_id['<END>']
+	for i, word in enumerate(word_list):
+		id_s[i] = vocab_map_id.get(word, 0)
+	return id_s
+
+def build_vocab(sentences_list):
+	words = []
+	for sentence in sentences_list:
+		words += sentence.split()
+	word_counter = Counter(words)
+	vocab = word_counter.keys()
+	vocab = ['<UNK>'] + vocab
+
+	vocab_id_map = {x: i for i, x in enumerate(vocab)}
+	return vocab, vocab_id_map
+
+
+
 
 def embedded_word(word, stop_word, word_vector, word_vector_size):
 	if word in word_vector and word not in stop_word:
